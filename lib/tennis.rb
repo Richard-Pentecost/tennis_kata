86d6c55233870df61c7c4bebb7553dc49057bd78
Richard Pentecost <richard.pentecost@madetech.com>
2020-09-17 15:39:06 +0100

    show score test failing

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,5 +1,11 @@
 class Tennis
-  def self.some_method
-    "success"
+  attr_accessor :game_score
+
+  def initialize
+    @game_score = { player1: 0, player2: 0}
+  end
+
+  def show_score
+    'hello'
   end
 end
--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,7 +1,17 @@
 require_relative '../lib/tennis'
 
-describe Tennis do 
-  it 'should test methods in tennis.rb' do
-    expect(Tennis.some_method).to eq('success')
+describe 'Tennis' do 
+  it "initialize a game with player1's game scores being 0" do
+    expect(Tennis.new.game_score[:player1]).to eq(0)
   end
+
+  it "initialize a game with player2's game scores being 0" do
+    expect(Tennis.new.game_score[:player2]).to eq(0)
+  end
+
+  it 'shows this game score of the 2 players' do
+    tennis = Tennis.new
+    expect(tennis.show_score()).to eq("Player 1: #{tennis[:players1]} - #{tennis[:player2]} :Player 2")
+  end
+
 end
